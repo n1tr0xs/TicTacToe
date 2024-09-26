f9b6cffdd0a4c0ff56f55305166caf32e5bd5cf5
Programmist <niko4pok@gmail.com>
2024-09-26 16:49:29 +0300

    dirty commit

--- main.py
+++ main.py
@@ -4,74 +4,22 @@ except ImportError:
     print('''
 Some required Python modules are not installed:
     - pygame
-You can use `pip install -r requirements.txt`
+You can use `pip install -r requirements.txt` to install all required modules.
 ''')
 
 from constants import *
 from board import Board
-
-def draw_X(surface: pygame.surface.Surface, center: tuple[int, int]) -> None:
-    '''
-    surface - surface to draw symbol
-    center  - center coordinates for the symbol
-    '''
-    x, y = center
-    coef = CELL_SIZE // 2 * .8
-    pygame.draw.line(
-        surface, "white",
-        (x - coef, y - coef),
-        (x + coef, y + coef)
-    )
-    pygame.draw.line(
-        surface, "white",
-        (x + coef, y - coef),
-        (x - coef, y + coef)
-    )
+from renderer import Renderer
 
 
-def draw_0(surface: pygame.surface.Surface, center: tuple[int, int]) -> None:
-    '''
-    surface - surface to draw symbol
-    center  - center coordinates for the symbol
-    '''
-    coef = CELL_SIZE // 2 * .85
-    pygame.draw.circle(
-        surface, "white",
-        center, coef, 1
-    )
-
-
-def draw_board(surface: pygame.surface.Surface, board: list) -> None:
-    # draws vertical lines
-    for i in range(1, CELLS):
-        pygame.draw.line(
-            surface, "white",
-            (CELL_SIZE * i, 0), (CELL_SIZE * i, CELL_SIZE * CELLS)
-        )
-    # draws horizontal lines
-    for i in range(1, CELLS):
-        pygame.draw.line(
-            surface, "white",
-            (0, CELL_SIZE * i), (CELL_SIZE * CELLS, CELL_SIZE * i)
-        )
-
-    for i in range(board.size):
-        for j in range(board.size):
-            x = CELL_SIZE * i + CELL_SIZE // 2
-            y = CELL_SIZE * j + CELL_SIZE // 2
-            match board.get(i, j):
-                case 'X':
-                    draw_X(surface, (x, y))
-                case '0':
-                    draw_0(surface, (x, y))
-
+Colorable = pygame.Color | str | int | tuple[int, int, int, [int]]
 
 pygame.init()
-
 screen = pygame.display.set_mode(WINDOW_SIZE)
 clock = pygame.time.Clock()
 
 board = Board()
+renderer = Renderer(screen, "white")
 
 running = True
 while running:
@@ -84,17 +32,18 @@ while running:
             break
         elif event.type == pygame.MOUSEBUTTONUP:
             # converting coordinates to cells
-            i, j = map(lambda a: a // CELL_SIZE, event.pos)
+            x, y = event.pos
+            i, j = map(lambda a: a // CELL_SIZE, (x, y))
             board.turn(i, j)
 
-    draw_board(screen, board)
+    renderer.draw_board(board)
     pygame.display.flip()
 
-    if board.is_draw():
-        print('Draw!')
-        running = False
     if (winner := board.is_winner()):
         print(f'{winner} won!')
+        running = False
+    if board.is_draw():
+        print('Draw!')
         running = False
 
     clock.tick(10)
---
+++ renderer.py
@@ -0,0 +1,78 @@
+import pygame
+class Renderer:
+    '''
+    Object for rendering game.
+    '''
+
+    def __init__(self, surface: pygame.surface.Surface, color: Colorable = "white"):
+        '''
+        Creates Rendere object.
+
+        surface - pygame.surface.Surface
+        color - pygame.Color or str or int or tuple[int, int, int, [int]]
+        '''
+        self.surface = surface
+        self.color = pygame.Color(color)
+
+    def draw_board(self, board: Board, color: Colorable = "white") -> None:
+        '''
+        Draws board on screen.
+
+        board - Board object.
+        color - pygame.Color or str or int or tuple[int, int, int, [int]]
+        '''
+        # draws vertical lines
+        for i in range(1, CELLS):
+            pygame.draw.line(
+                self.surface, color or self.color,
+                (CELL_SIZE * i, 0), (CELL_SIZE * i, CELL_SIZE * CELLS)
+            )
+        # draws horizontal lines
+        for i in range(1, CELLS):
+            pygame.draw.line(
+                self.surface, color or self.color,
+                (0, CELL_SIZE * i), (CELL_SIZE * CELLS, CELL_SIZE * i)
+            )
+        # draws signs on the board
+        for i in range(board.size):
+            for j in range(board.size):
+                x = CELL_SIZE * i + CELL_SIZE // 2
+                y = CELL_SIZE * j + CELL_SIZE // 2
+                match board.get(i, j):
+                    case 'X':
+                        self.draw_X(x, y)
+                    case '0':
+                        self.draw_0((x, y))
+
+    def draw_X(self, x: int, y: int, color: Colorable = None) -> None:
+        '''
+        Draws X with center in given coordinates.
+
+        x - int
+        y - int
+        color - pygame.Color or str or int or tuple[int, int, int, [int]]
+        '''
+        coef = CELL_SIZE // 2 * .8
+        pygame.draw.line(
+            self.surface, color or self.color,
+            (x - coef, y - coef),
+            (x + coef, y + coef)
+        )
+        pygame.draw.line(
+            self.surface, color or self.color,
+            (x + coef, y - coef),
+            (x - coef, y + coef)
+        )
+
+    def draw_0(self, center: tuple[int, int], color: Colorable = None) -> None:
+        '''
+        Draws 0 in given `center`.
+
+        center  - center coordinates for the symbol
+        color - pygame.Color or str or int or tuple[int, int, int, [int]]
+        '''
+        coef = CELL_SIZE // 2 * .85
+        pygame.draw.circle(
+            self.surface, color or self.color,
+            center, coef, 1
+        )
